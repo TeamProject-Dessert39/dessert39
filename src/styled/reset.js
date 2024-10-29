4d931c3d8638b37a4d7790b93d0ef29fc53690e1
injun <jun2000005@naver.com>
2024-10-29 16:22:27 +0900

    first commit

---
+++ src/styled/reset.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* font */
+  /* 
+  font-family: 'Noto Sans KR', sans-serif;  
+  font-family: "Lexend Deca", sans-serif;
+  */
+  /* color */
+  :root {
+    
+  }
+  /* 공통 */
+  .inner{
+    width: 95%;
+    max-width: 1440px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    position: relative;
+  }
+  /* other styles */
+  body {
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 1.6;
+    font-family: 'Noto Sans KR', sans-serif;  
+    color: #1c1c1c;
+  }
+  a {
+    text-decoration: none;
+    color: #1c1c1c;
+  }
+  li {
+    list-style: none;
+  }
+  img{
+    vertical-align: top;
+  }
+  table {
+    width: 100%;
+  }
+`;
+export default GlobalStyle;
