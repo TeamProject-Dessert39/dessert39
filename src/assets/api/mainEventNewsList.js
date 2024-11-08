1875bfb5e73be7c1b39fc3b567ef85cd83c281cd
injun <jun2000005@naver.com>
2024-11-08 12:37:29 +0900

    EventNews update_20241108_1237

--- src/assets/api/mainEventNewsList.js
+++ src/assets/api/mainEventNewsList.js
@@ -1,43 +1,12 @@
 export default [
-    { id: 1, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 2, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 3, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 4, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 5, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 6, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 7, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 8, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 9, title: '', date: '', photoUrl: '', linkUrl: '' },
-    { id: 10, title: '', date: '', photoUrl: '', linkUrl: '' },
+    { id: 1, title: '[24년 8월 신메뉴]제로 뱅쇼, 제로 샹그리아 출시!', date: '2024-08-26', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_01.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=179' },
+    { id: 2, title: '[24년 8월 신메뉴]설탕무첨가 요거트 아이스크림 음료 5종 출시!', date: '2024-08-14', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_02.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=178' },
+    { id: 3, title: '[24년 11월 신메뉴]흑당라떼 2종 출시!', date: '2024-11-05', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_03.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=188' },
+    { id: 4, title: '[24년 10월신메뉴]가격, 가격, 맛 모두 &#39;착한바닐라라떼&#39; 출시!', date: '2024-10-24', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_04.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=187' },
+    { id: 5, title: '[24년 10월 신메뉴]노슈거 바스크 치즈 케이크 출시!', date: '2024-10-08', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_05.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=186' },
+    { id: 6, title: '[24년 10월 신메뉴]바나나 콜드브루 라떼 출시!', date: '2024-10-07', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_06.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=185' },
+    { id: 7, title: '[24년 9월 신메뉴]스페셜티 원두 변경', date: '2024-09-30', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_07.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=184' },
+    { id: 8, title: '[24년 9월 신메뉴]설탕무첨가 허니자몽 음료4종 출시!', date: '2024-09-19', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_08.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=183' },
+    { id: 9, title: '[24년 9월 신메뉴]노슈거 크림치즈 크로칸슈 출시!', date: '2024-09-19', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_09.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=182' },
+    { id: 10, title: '[24 8월 신메뉴]리치 블랜딩 음료 4종 출시!', date: '2024-08-29', photoUrl: 'https://github.com/TeamProject-Dessert39/dataCenter/blob/master/images/main/EventNewsList/EventNewsList_10.png?raw=true', linkUrl: 'https://dessert39.com/bbs/board.php?bo_table=Information&wr_id=181' }
 ]
-/*
-[24년 8월 신메뉴]제로<br />뱅쇼, 제로 샹그리아 출시!
-2024-08-26
-
-[24년 8월 신메뉴]<br />설탕무첨가 요거트 아이스크림 음료 5종 출시! 
-2024-08-14
-
-[24년 11월 신메뉴]<br />흑당라떼 2종 출시!
-2024-11-05
-
-[24년 10월신메뉴]가격,<br />가격, 맛 모두 '착한바닐라라떼' 출시!                                    
-2024-10-24
-
-[24년 10월 신메뉴]<br />노슈거 바스크 치즈 케이크 출시!                                    
-2024-10-08
-
-[24년 10월 신메뉴]<br />바나나 콜드브루 라떼 출시!                                    
-2024-10-07
-
-[24년 9월 신메뉴]<br />스페셜티 원두 변경
-2024-09-30
-
-[24년 9월 신메뉴]<br />설탕무첨가 허니자몽 음료4종 출시!                                    
-2024-09-19
-
-[24년 9월 신메뉴]노슈거<br /> 크림치즈 크로칸슈 출시!
-2024-09-19
-
-[24 8월 신메뉴]리치<br />블랜딩 음료 4종 출시!
-2024-08-29
-*/
