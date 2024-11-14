fffbe788b0a8ae20a45da509ff5d8f9e2a9d03d7
injun <jun2000005@naver.com>
2024-11-14 16:35:58 +0900

    brand Update_20241114_1635

--- src/components/brand/index.js
+++ src/components/brand/index.js
@@ -8,3 +8,6 @@ export { default as Support } from './Support';
 export { default as ConfectioneryCenter } from './ConfectioneryCenter'
 export { default as ConfectioneryCenterSub1 } from './ConfectioneryCenterSub1'
 export { default as ConfectioneryCenterSub2 } from './ConfectioneryCenterSub2'
+
+// Organizational Chart
+export { default as OrganizationalChart } from './OrganizationalChart'
